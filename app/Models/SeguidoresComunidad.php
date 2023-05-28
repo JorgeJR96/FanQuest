f9c53ea5a5aa82da4f2ab2dbba100b75f6590eb9
JorgeJR96 <75904547+JorgeJR96@users.noreply.github.com>
2023-05-29 00:38:04 +0200

    Limpieza migraciones y creacion de modelos de bbdd

--- app/Models/SeguidoresComunidad.php
+++ app/Models/SeguidoresComunidad.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class todo extends Model
+class SeguidoresComunidad extends Model
 {
     use HasFactory;
+    protected $table = 'seguidorescomunidad' ;
 }

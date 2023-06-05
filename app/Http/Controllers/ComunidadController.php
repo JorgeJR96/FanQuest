fa0ea79da9e7340ad82a48f49b53d9fa2c9c5ded
JorgeJR96 <75904547+JorgeJR96@users.noreply.github.com>
2023-06-05 23:15:48 +0200

    .

---
+++ app/Http/Controllers/ComunidadController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ComunidadController extends Controller
+{
+    //
+    public function store(Request $request){
+
+        $request->validate([
+            ''
+        ]);
+    }
+}
